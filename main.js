d9d49f6da43fa81a2bf3705f94e4b59878fec061
Nadia <nadiyaklokova@gmail.com>
2021-12-27 14:53:02 +0100

    animaciones

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+     document.getElementById("nav").style.background = " rgba(255,255,255,.9)"; 
+     document.getElementById("logo-derecha").style.display = "none"; 
+     document.getElementById("logo").style.display = "none"; 
+     document.getElementById("logo-extra").style.display = "block"; 
+
+
+  } else {
+    document.getElementById("nav").style.background = "linear-gradient(90deg, rgba(255,255,255,1) 19%, rgba(181,237,219,1) 56%)";
+    document.getElementById("logo-derecha").style.display = "block"; 
+    document.getElementById("logo").style.display = "block"; 
+    document.getElementById("logo-extra").style.display = "none"; 
+  }
+}
